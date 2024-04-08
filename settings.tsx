3dde47408778f6debeace47f142438ddaa80589f
Teguh Wijiyanto <teguh.wijiyanto@gmail.com>
2024-04-08 09:25:11 +0700

    Updates

---
+++ settings.tsx
@@ -0,0 +1,13 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+
+export const mintText = [
+  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
+  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" }
+];
+
+//header image in the ui. replace with your own
+export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+
+//website title
+export const headerText = "MarkSackerberg's mint UI";
